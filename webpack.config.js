9c3d774a519c1c1a6d9f167d711a6c2765e53b35
kmsheng <kmsh3ng@gmail.com>
2022-03-24 23:51:50 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,32 @@
 const path = require('path')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'js/[name].js'
+    path: path.resolve(__dirname, './dist'),
+    filename: 'main.js',
+    clean: true
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    })
+  ],
   module: {
     rules: [
       {
+        test: /\.(js|jsx)$/i,
+        loader: 'babel-loader'
+      },
+      {
         test: /\.css$/i,
-        use: [MiniCssExtractPlugin.loader, 'css-loader']
+        use: ['style-loader', 'css-loader', 'postcss-loader']
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+        type: 'asset'
       }
     ]
-  },
-  plugins: [
-    new MiniCssExtractPlugin({
-      filename: 'css/[name].css',
-    }),
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-      filename: 'index.html',
-    })
-  ]
+  }
 }
